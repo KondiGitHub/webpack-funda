d43498ddb13f125a3867793225dbfe9ce2e6a338
KondiGitHub <haichary@yahoo.co.in>
2017-12-28 01:11:07 -0500

    babel loader configuration

---
+++ login.es6
@@ -0,0 +1,7 @@
+let login = (username,password) => {
+        if( username !== 'Admin' || password !== 'Chary') {
+            console.log("Incorrect login");
+}
+};
+
+login("Admin","venkat");
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-    entry: ['./utils','./app.js'],
+    entry: ['./utils.js','./app.js'],
     output : {
         filename: 'bundle.js'
     },
-    watch : true
+    watch : true,
+    module : {
+        loaders : [
+            {
+                test :/\.es6$/,
+                exclude : /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    resolve:{
+        extensions:['.js','.es6']
+    }
 };
