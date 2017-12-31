97c835923152f0557725bc65f0efd143627bcf1f
Venkateswara Chary <venkateswarachary.m@lexisnexis.com>
2017-12-30 20:16:47 -0500

    custom loaders

---
+++ loaders/strip.js
@@ -0,0 +1,9 @@
+var stripComments = require('strip-json-comments');
+
+
+module.exports = function (source) {
+    this.cacheable();
+    console.log("source",source);
+    console.log('stripSource',stripComments(source));
+    return stripComments(source);
+};
--- public/js/app.js
+++
@@ -1,3 +0,0 @@
-
-console.log("App loaded");
-require('../css/style.css');
